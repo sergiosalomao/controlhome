d9013661b4a86ceb3b584c9faa7376b6ea4e4968
Sergio Salomao <sergio.salomao@fabricainfo.com>
2019-09-16 14:06:49 -0300

    ajuste

--- app/Views/configuracao/configuracao.php
+++ app/Views/configuracao/configuracao.php
@@ -15,9 +15,8 @@
               <td style="border:none"><a href="temperatura.php"><img src="../public/images/temperatura.png" width="86pxpx" height="86pxpx"></a></td>
               <td style="border:none"><a href="iluminacao"><img src="../public/images/iluminacao.png" width="86pxpx" height="86pxpx"></a></td> -->
                             <td style="border:none"><a href="configuracao/usuarios"><i class="fas fa-user-cog icon-home"></i></a></td>
-                            <td style="border:none"><a href="configuracao/ambientes"><i class="fas fas fa-microchip icon-home"></i></a></td>
-                            <td style="border:none"><a href="configuracao/reservatorios"><i class="fas fa-water  icon-home"></i></a></td>
-                        </tr>
+                            <td style="border:none"><a href="configuracao/ambientes"><i class="far fa-layer-plus icon-home"></i></a></td>
+                            <td style="border:none"><a href="configuracao/reservatorios"><i class="fas fa-water icon-home"></i></a></td>
                     </table>
                 </div>
             </div>
