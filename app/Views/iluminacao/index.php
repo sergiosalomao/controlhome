47ecd7a93c89d0fb4fb945aac1c76dbe194e1d4f
Sergio Salomao <sergio.salomao@fabricainfo.com>
2019-09-16 14:42:53 -0300

    ajuste

--- app/Views/iluminacao/index.php
+++ app/Views/iluminacao/index.php
@@ -12,7 +12,7 @@ use App\Models\AmbientesModel\AmbientesModel;
                 <div>
                 </div>
             </div>
-
+            <div class="lista-titulo"><i class="fas fa-dice-d6" style="margin-left:10px"></i><span style="margin-left:10px"> Interruptores </span></div>
             <div class="container dashboard-configuracao">
                 <table class="table table-hover table-striped">
 
